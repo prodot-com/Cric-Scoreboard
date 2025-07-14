3ea0bc338068fca945d233a203f8fe58a1528d16
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-15 01:27:00 +0530

    Added them in index file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
 import React from 'react'
-import Header from './Component/FrontPage/Header/Header'
-import MatchCreate from './Component/FrontPage/MatchCreate/MatchCreate'
-import Title from './Component/Title/Title'
-import Matches from './Component/FrontPage/Matches/Matches'
+import Index from './Component/FrontPage'
 
 const App = () => {
   return (
-    <div className="min-h-screen w-full bg-white relative">
-{/* Magenta Orb Grid Background */}
-<div
- className="absolute inset-0 z-0"
- style={{
-   background: "white",
-   backgroundImage: `
-     linear-gradient(to right, rgba(71,85,105,0.15) 1px, transparent 1px),
-     linear-gradient(to bottom, rgba(71,85,105,0.15) 1px, transparent 1px),
-     radial-gradient(circle at 50% 60%, rgba(236,72,153,0.15) 0%, rgba(168,85,247,0.05) 40%, transparent 70%)
-   `,
-   backgroundSize: "40px 40px, 40px 40px, 100% 100%",
- }}
-/>
-  {/* Your Content/Components */}
-  <div className='relative z-10'>
-    <Header/>
-    <MatchCreate/>
-    <Title text={`Live Match Now`}/>
-    <Matches/>
-  </div>
-</div>
+    <Index/>
   )
 }
 
