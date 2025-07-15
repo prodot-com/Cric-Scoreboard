d9e813706a1964db0f41760827903fb836456769
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-16 01:46:24 +0530

    added scoreboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Index from './Component/FrontPage'
 import Creation from './Component/CreationPage/Creation'
 import { BrowserRouter, Routes, Route , Link} from 'react-router-dom'
 import Toss from './Component/TossPage/UpperSection/Toss'
+import AdminPage from './Component/AdminPage/AdminPage'
+
 
 
 const App = () => {
@@ -12,6 +14,7 @@ const App = () => {
         <Route path='/' element={<Index/>}/>
         <Route path='/creation' element={<Creation/>}/>
         <Route path='/toss' element={<Toss/>}/>
+        <Route path='/admin' element={<AdminPage/>}/>
     </Routes>
     </BrowserRouter>
   )
