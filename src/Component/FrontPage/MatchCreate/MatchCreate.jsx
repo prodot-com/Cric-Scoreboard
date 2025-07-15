26cca5ecd53e205e5f993ff769c3fb757a94d6d8
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-15 23:38:00 +0530

    .

--- src/Component/FrontPage/MatchCreate/MatchCreate.jsx
+++ src/Component/FrontPage/MatchCreate/MatchCreate.jsx
@@ -13,7 +13,7 @@ const MatchCreate = () => {
     <>
     <div onClick={routeChange}>
             <div className=' bg-linear-to-r from-gray-800 via-blue-700 to-gray-900 flex justify-center items-center 
-        h-20 rounded-3xl mx-14  text-3xl font-bold text-white hover:shadow-lg shadow-blue-700/100 delay-75'>
+        h-20 rounded-3xl mx-14  text-3xl font-bold text-white hover:shadow-lg shadow-blue-700/100 delay-75 cursor-pointer'>
             <h3>Create a Match</h3> 
         </div>
     </div>
