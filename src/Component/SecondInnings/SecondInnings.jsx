edc7c527c110d14d5a3a57e1c326031dd28ae3b4
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-26 00:15:31 +0530

    .

--- src/Component/SecondInnings/SecondInnings.jsx
+++ src/Component/SecondInnings/SecondInnings.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
 
 const SecondInnings = () => {
+    let navigate = useNavigate()
+
+    const [firstInningsDetails, setFirstInningsDetails] = useState({})
+    const [target, setTarget] = useState(0)
+
+    useEffect(()=>{
+
+        const firstInningsDetails = JSON.parse(localStorage.getItem('firstInningsDetails'))
+        console.log(firstInningsDetails)
+        if(!firstInningsDetails){
+            navigate('/admin')
+        }else{
+            setFirstInningsDetails(firstInningsDetails)
+        }
+
+    },[navigate])
+
+    useEffect(()=>{
+      console.log(firstInningsDetails)
+      setTarget(firstInningsDetails.runs + 1)
+    },[firstInningsDetails])
+
+
   return (
     <div>
       <h3>2nd Innings</h3>
+      <h4>{`Target: ${target}`}</h4>
+      <h4>{`battingTeam: ${firstInningsDetails.battingteam}`}</h4>
     </div>
   )
 }
