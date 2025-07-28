2102042e48391285261f7f9d9bd5087d56c9e49b
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-29 00:05:20 +0530

    .

--- Backend/server.js
+++ Backend/server.js
@@ -1,34 +1,42 @@
 import express from 'express'
-import {createServer} from 'http'
+import { createServer } from 'http'
 import { Server } from 'socket.io'
 import cors from 'cors'
 
 const app = express()
-app.use(cors())
+
+// Allow multiple origins
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ['GET', 'POST'],
+    credentials: true
+}))
 
 const httpServer = createServer(app)
 
 const io = new Server(httpServer, {
-    cors:{
-        origin: "http://localhost:5173",
-        methods: ['GET', 'POST']
+    cors: {
+        origin: allowedOrigins,
+        methods: ['GET', 'POST'],
+        credentials: true
     }
 })
 
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
     console.log('User Connected: ', socket.id)
 
-    socket.on('message', (data)=>{
+    socket.on('message', (data) => {
         console.log('Message received: ', data)
         io.emit('message', data)
     })
 })
 
-
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Socket.io backend running")
 })
 
-httpServer.listen(9000, ()=>{
+httpServer.listen(9000, () => {
     console.log('server running on localhost:9000')
 })
--- LiveFrontend/src/App.jsx
+++ LiveFrontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import LivePage from './Component/LivePage/LivePage'
 
 const App = () => {
   return (
     <>
       <h3>Live Page</h3>
+      <LivePage/>
     </>
   )
 }
