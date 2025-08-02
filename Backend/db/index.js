6daf0847d4cb44be3078d3b8713c6837ec67e0ab
Probal Ghosh <probalghoshofficial@gmail.com>
2025-08-02 16:26:06 +0530

    Connected mongoDB

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,9 +1,9 @@
-import moongoose from 'moongoose'
+import mongoose from 'mongoose'
 import 'dotenv/config'
 
 const connectDB = async ()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/Match`)
+        const connectionInstance = await mongoose.connect(`mongodb+srv://Probal2311:Probal2311@probal0.gftxuv1.mongodb.net//Match`)
         console.log(`\n MongoDB connected ☑️ \n DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MongoDb connection failed")
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 import cors from 'cors'
+import connectDB from './db/index.js'
 
 const app = express()
 
@@ -15,6 +16,8 @@ app.use(cors({
 }))
 
 const httpServer = createServer(app)
+
+connectDB()
 
 const io = new Server(httpServer, {
     cors: {
