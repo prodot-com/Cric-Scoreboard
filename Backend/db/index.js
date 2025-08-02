e1eb75899902e9a0c55776df0db4c635c26c869e
Probal Ghosh <probalghoshofficial@gmail.com>
2025-08-02 23:21:16 +0530

    .

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose'
 import 'dotenv/config'
+import DB_NAME from '../Constants.js'
 
 const connectDB = async ()=>{
     try {
-        const connectionInstance = await mongoose.connect(`mongodb+srv://Probal2311:Probal2311@probal0.gftxuv1.mongodb.net/Match`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n MongoDB connected ☑️ \n DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MongoDb connection failed")
