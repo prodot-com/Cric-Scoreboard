2a11da5ad204fefa51b063161ea5fb24408aa0ba
Probal Ghosh <probalghoshofficial@gmail.com>
2025-08-05 18:05:03 +0530

    upgraded the match Model

--- Backend/model/Match.model.js
+++ Backend/model/Match.model.js
@@ -1,12 +1,23 @@
 import mongoose from 'mongoose';
 
+const inningsSchema = new mongoose.Schema({
+  battingTeam: {type: String},
+  bowlingTeam: {type: String},
+  runs: {type: Number, default: 0},
+  balls: {type: Number, default: 0},
+  wickets: {type: Number, default: 0},
+},{_id: false})
+
 const matchSchema = new mongoose.Schema({
   name: { type: String, required: true },
   team1: { type: String, required: true },
   team2: { type: String, required: true },
   over: { type: Number, required: true },
   tossWinner: {type: String},
-  decision: {type: String}
+  decision: {type: String},
+  firstSummary: inningsSchema,
+  secondSummary: inningsSchema,
+  
 },{timestamps:true},{
   collection: 'matches'
 });
